c104192ca506348496d7cedcdd67c9dca61844e5
Shashi Kumar Sah <shashichander009@gmail.com>
2019-08-04 16:23:12 +0530

    added modules

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const check = require("./test");
-console.log(check.sum(44, 33));
-console.log("Shashi Kumar Sah");
+var rect = require("./test");
+
+// var rect = {
+//   perimeter: (x, y) => 2 * (x + y),
+//   area: (x, y) => x * y
+// };
+
+function solvereact(l, b) {
+  console.log(`Solving for area of ${l} and ${b}`);
+  if (l <= 0 || b <= 0) {
+    console.log("Both L and B should be greater than 0");
+  } else {
+    console.log(" The perimter is " + rect.perimeter(l, b));
+    console.log(" The area is " + rect.area(l, b));
+  }
+}
+
+solvereact(4, 5);
+solvereact(0, 6);
+solvereact(-3, 5);
+solvereact(1, 51);
+solvereact(4, 0);
--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-exports.sum = (x, y) => x + y;
+exports.perimeter = (x, y) => 2 * (x + y);
 exports.area = (x, y) => x * y;
