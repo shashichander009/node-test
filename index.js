10b9655ab8dd72362cbe5f92a0bc30a0a773918b
Shashi Kumar Sah <shashichander009@gmail.com>
2019-08-04 15:33:03 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const check = require("./test");
+console.log(check.sum(44, 33));
+
+console.log(check.area(333, 33));
---
+++ test.js
@@ -0,0 +1,2 @@
+exports.sum = (x, y) => x + y;
+exports.area = (x, y) => x * y;
